be0c3b0663cad23191feb842980898f3848cf864
Enrique Gonzalez <enrique@saucelabs.com>
2019-06-05 14:37:50 -0700

    change simple check to "sauce" instead of "ping" and add close event Prior to this change, ping was used to get something other than an echo message. This might be confusing since there are ping and pong events on ws so changed to sauce and labs Additionally, adding a close event to know when the client disconnects.

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ const WebSocket = require('ws');
 
 var port = process.env.PORT || 8080
 
-const wss = new WebSocket.Server({ port });
+const wss = new WebSocket.Server({ port }, () => {
+    console.log("Server started on port %d", port);
+});
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
 
-    if (message.toLowerCase() === "ping") {
-        ws.send("pong");
-    } else {
-        ws.send(message);
-    }
+        if (message.toLowerCase() === "sauce") {
+            ws.send("labs");
+        } else {
+            ws.send(message);
+        }
 
-  });
+    });
+
+    ws.on('close', (code, reason) => {
+        console.log("Closing connection")
+        console.log("  Code: %s Reason: %s", code, reason)
+    });
 });
